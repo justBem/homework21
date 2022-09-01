8f3a99c7f2f7aa7e39618356bb671b6b6f4ab09b
Admin <bermetsaadat@gmail.com>
2022-09-01 13:51:13 +0600

    hin

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         FileWriter writer=new FileWriter("alfavit.txt");
-         for(char i='A',j='a';i<'Z';i++){
-             writer.write(i+" ");
+        int d=0;
+        int e=26;
+         for(char i='A',j='a';i<='Z';i++){
+             writer.write(d+++":"+i+" ");
              writer.write(j);
              j++;
              writer.write("\n");
          }
          for(int c=1;c<=9;c++){
-             writer.write(c+"\n");
+             writer.write(e+++":"+c+"\n");
          }
          writer.close();
         FileReader reader=new FileReader("alfavit.txt");
